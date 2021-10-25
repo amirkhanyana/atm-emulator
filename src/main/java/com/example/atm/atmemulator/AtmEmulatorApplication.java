4846e7ea8c22131839d340ac3d78b1f0ed3edaf6
homo-logicus <homo.logicuss@gmail.com>
2021-10-25 20:14:44 +0400

    Initial commit

---
+++ src/main/java/com/example/atm/atmemulator/AtmEmulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.atm.atmemulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmEmulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmEmulatorApplication.class, args);
+	}
+
+}
